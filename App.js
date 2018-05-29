cee9cc2dc4bb913985f63acf7639f5332752c651
SathishSaminathan <Sathishcse1975@gmail.com>
2018-05-29 12:48:37 +0530

    Push Notification Implementation

--- App.js
+++ App.js
@@ -19,8 +19,49 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+import FCM, { NotificationActionType } from "react-native-fcm";
+
+export default class App extends Component {
+
+  async componentDidMount() {
+    registerAppListener(this.props.navigation);
+    FCM.getInitialNotification().then(notif => {
+      this.setState({
+        initNotif: notif
+      });
+      if (notif && notif.targetScreen === "detail") {
+        setTimeout(() => {
+          this.props.navigation.navigate("Detail");
+        }, 500);
+      }
+    });
+
+    try {
+      let result = await FCM.requestPermissions({
+        badge: false,
+        sound: true,
+        alert: true
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    FCM.getFCMToken().then(token => {
+      console.log("TOKEN (getFCMToken)", token);
+      this.setState({ token: token || "" });
+    });
+
+    if (Platform.OS === "ios") {
+      FCM.getAPNSToken().then(token => {
+        console.log("APNS TOKEN (getFCMToken)", token);
+      });
+    }
+
+    // topic example
+    // FCM.subscribeToTopic('sometopic')
+    // FCM.unsubscribeFromTopic('sometopic')
+  }
+
   render() {
     return (
       <View style={styles.container}>
